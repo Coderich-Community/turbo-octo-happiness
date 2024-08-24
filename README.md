d86f5949f3c5715e68ebff0954092d477cfd4041
Md Samer Ansari <smaitykhan@gmail.com>
2024-08-24 20:35:09 +0530

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1> AgriLink <h1>
+
+By Cyber Spartans
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+     <div className='text-green-300'>
+      Hello AgriLink Project
+     </div>
+    </>
+  )
+}
+
+export default App
