b6472b5c6f3197d496f328dba36e7e724d56f0b0
Priyansu Singh <priyansusingh3344@gmail.com>
2024-09-04 23:07:53 +0530

    contact

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -2,6 +2,7 @@
 
 export const ContactUs = () => {
   return (
+    <div>
     <div className="flex flex-col md:flex-row items-start">
       {/* Left Section - Contact Details */}
       <div className="bg-green-700 text-white p-8 md:w-1/3 w-full">
@@ -126,6 +127,7 @@ export const ContactUs = () => {
           ></iframe>
         </div>
       </div>
+    </div>
     </div>
   );
 };
