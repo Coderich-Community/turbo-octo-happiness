b36c62471bbc94ffaee93a372bfaa217269577ef
Md Samer Ansari <smaitykhan@gmail.com>
2024-09-04 23:49:52 +0530

    img add

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import About from "../components/About"
 import { ContactUs } from "../components/ContactUs"
 import { Contract } from "../components/Contract"
-import { Corousel } from "../components/corousel"
+import { Corousel } from "../components/Corousel"
 import { Navbar } from "../components/Navbar"
 
 export const Home = () =>{
@@ -10,12 +10,11 @@ export const Home = () =>{
 
     return <div className="">
            <Navbar/>
-           <Corousel/> 
+           <Corousel /> 
            <About/>
            <div className=" flex justify-center">
            <Contract/>
            </div>
-           <ContactUs/>
-         
+           <ContactUs/>  
          </div>
 }
