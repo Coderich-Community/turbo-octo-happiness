35a692d11e35848d9e9e3feb0c956c64c09ca34f
Priyansu Singh <priyansusingh3344@gmail.com>
2024-09-05 01:03:54 +0530

    fdfd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,21 @@
-
-
-import {About} from "../components/About"
+import About from "../components/About"
 import { ContactUs } from "../components/ContactUs"
 import { Contract } from "../components/Contract"
 import { Corousel } from "../components/Corousel"
-
+import { Footer } from "../components/Footer"
 import { Navbar } from "../components/Navbar"
 
-export const Home = () =>{
-
-    return <div className="">
-           <Navbar/>
-           <Corousel/> 
-           <About/>
-           <div className=" flex justify-center pb-20">
-           <Contract/>
-           </div>
-           <ContactUs/>
-          <div className="">
-          </div>
-         </div>
+export const Home = () => {
+    return (
+        <div className="">
+            <Navbar />
+            <Corousel />
+            <About />
+            <div className="flex justify-center">
+                <Contract />
+            </div>
+            <ContactUs />
+            <Footer />
+        </div>
+    )
 }
