cc4d33726686aa576ce5a9e960d229c07075fbc0
Md Samer Ansari <smaitykhan@gmail.com>
2024-09-04 23:41:39 +0530

    init

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,8 @@
 
-<<<<<<< HEAD
 import About from "../components/About"
 import { ContactUs } from "../components/ContactUs"
 import { Contract } from "../components/Contract"
 import { Corousel } from "../components/Corousel"
-=======
-import { Corousel } from "../components/corousel"
-import Footer from "../components/Footer/Footer"
->>>>>>> 1c6fcd714a5d33883b6d6eb82771c08e082203f8
 import { Navbar } from "../components/Navbar"
 
 export const Home = () =>{
@@ -15,7 +10,6 @@ export const Home = () =>{
 
     return <div className="">
            <Navbar/>
-<<<<<<< HEAD
            <Corousel/> 
            <About/>
            <div className=" flex justify-center">
@@ -23,10 +17,5 @@ export const Home = () =>{
            </div>
            <ContactUs/>
          
-=======
-           <Corousel />
-           <Footer />
-           
->>>>>>> 1c6fcd714a5d33883b6d6eb82771c08e082203f8
          </div>
 }
